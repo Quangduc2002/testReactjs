38e91ed9b6035da8222b4f73d98bf21f579dacc6
Quangduc2002 <phamquangduc110@gmail.com>
2024-06-03 19:52:14 +0700

    update refreshToken

--- src/Services/CutomizeAxios.js
+++ src/Services/CutomizeAxios.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import axios from 'axios';
 const instance = axios.create({
     // baseURL: 'https://agiletech-test-api.zeabur.app',
@@ -10,6 +11,35 @@ instance.interceptors.request.use(
         return config;
     },
     function (error) {
+        return Promise.reject(error);
+    },
+);
+let isRefreshing = false;
+instance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+            const refreshToken = localStorage.getItem('refreshToken');
+            return new Promise((resolve, reject) => {
+                axios
+                    .post('/auth/refreshToken', { token: refreshToken })
+                    .then(({ data }) => {
+                        localStorage.setItem('accessToken', data.accessToken);
+                        instance.defaults.headers.common['Authorization'] = `bearer ${data.accessToken}`;
+                        originalRequest.headers['Authorization'] = `Bearer ${data.accessToken}`;
+
+                        resolve(instance(originalRequest));
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    })
+                    .then(() => {
+                        isRefreshing = false;
+                    });
+            });
+        }
+
         return Promise.reject(error);
     },
 );
